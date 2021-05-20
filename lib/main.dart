8360a343e1a475c2e921b1e1a5b2a402967f219f
charith08 <charithsaivadla@gmail.com>
2021-05-20 10:38:25 +0530

    feat: Quiz and Result widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './question.dart';
-import './answer.dart';
+import './quiz.dart';
+import './result.dart';
 
 void main() {
   runApp(MyApp());
@@ -20,7 +20,7 @@ class _MyAppState extends State<MyApp> {
 
   // const is compile-time constant
   // final is run-time constant, cannot change after run-time
-  final questions = const [
+  final _questions = const [
     {
       'questionText': 'What\'s your favorite colour?',
       'answers': ['Black', 'Red', 'Green', 'White'],
@@ -42,8 +42,8 @@ class _MyAppState extends State<MyApp> {
 
     print(_questionIndex);
 
-    if (_questionIndex < questions.length) {
-      print('We have more questions');
+    if (_questionIndex < _questions.length) {
+      print('We have more _questions');
     }
   }
 
@@ -54,19 +54,13 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text('Welcome screen'),
         ),
-        body: _questionIndex < questions.length
-            ? Column(
-                children: [
-                  Question(questions[_questionIndex]['questionText']),
-                  ...(questions[_questionIndex]['answers'] as List<String>)
-                      .map((answer) {
-                    return Answer(_answerQuestion, answer);
-                  }).toList()
-                ],
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
               )
-            : Center(
-                child: Text('You did it!'),
-              ),
+            : Result(),
       ),
     );
   }
