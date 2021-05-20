b1c62223e8fd7e8836c1862758992bac319fbfa3
charith08 <charithsaivadla@gmail.com>
2021-05-20 11:19:13 +0530

    feat: calculating a total score

--- lib/main.dart
+++ lib/main.dart
@@ -17,27 +17,45 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore =0;
 
   // const is compile-time constant
   // final is run-time constant, cannot change after run-time
   final _questions = const [
     {
-      'questionText': 'What\'s your favorite colour?',
-      'answers': ['Black', 'Red', 'Green', 'White'],
+      'questionText': 'Who\'s your favorite director?',
+      'answers': [
+        {'text': 'Tharun Bhascker', 'score': 10},
+        {'text': 'Rajamouli', 'score': 8},
+        {'text': 'Vivek Athreya', 'score': 6},
+        {'text': 'Anil Ravipudi', 'score': 2}
+      ],
     },
     {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+      'questionText': 'What\'s your favorite mobile game?',
+      'answers': [
+        {'text': 'PUBG', 'score': 10},
+        {'text': 'COD', 'score': 8},
+        {'text': 'AmongUs', 'score': 6},
+        {'text': 'Free Fire', 'score': 2}
+      ],
     },
     {
-      'questionText': 'What\'s your favorite code editor?',
-      'answers': ['Atom', 'Sublime', 'VSC', 'Notepad++'],
+      'questionText': 'What\'s your favorite food?',
+      'answers': [
+        {'text': '🍕', 'score': 10},
+        {'text': '🍔', 'score': 8},
+        {'text': '🍱', 'score': 6},
+        {'text': '🥐', 'score': 2}
+      ],
     },
   ];
 
-  void _answerQuestion() {
+  void _answerQuestion(int score) {
+
+    _totalScore += score;
     setState(() {
-      _questionIndex = _questionIndex + 1;
+      _questionIndex += 1;
     });
 
     print(_questionIndex);
@@ -52,7 +70,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcome screen'),
+          title: Text('Personality Test'),
         ),
         body: _questionIndex < _questions.length
             ? Quiz(
@@ -60,7 +78,7 @@ class _MyAppState extends State<MyApp> {
                 questionIndex: _questionIndex,
                 questions: _questions,
               )
-            : Result(),
+            : Result(_totalScore),
       ),
     );
   }
