73b1d3027063b16ec4b53e0c7e4d4d127692aa1a
Charith <42198220+charith08@users.noreply.github.com>
2021-05-23 14:01:09 +0530

    Removing deprecated buttons + Logic of the app (#5)
    * feat: Quiz and Result widgets
    
    * feat: calculating a total score
    
    * feat: reset test
    
    * feat: Removing deprecated buttons, logic of the app

--- lib/result.dart
+++ lib/result.dart
@@ -9,32 +9,55 @@ class Result extends StatelessWidget {
   //Getter
   String get resultPhrase {
     String resultText;
-    if (resultScore <= 8) {
-      resultText = 'Hey';
-    } else if (resultScore <= 12) {
-      resultText = 'Hello';
+    if (resultScore == 30) {
+      //max
+      resultText =
+          'Ichipadestav! 💯🔥 Tharun Bhascker annav chudu akkada nachinav. You are the real deal ❤️';
+    } else if (resultScore == 6) {
+      //min
+      resultText = 'Poyi F2 chusko po 🤮 Free fire UI em baguntadhi asalu?';
+    } else if (resultScore == 26 || resultScore == 24) {
+      //8*3
+      resultText = 'DCP Padmanabha Simha 🔥🔥🔥🔥';
+    } else if (resultScore == 22) {
+      resultText = 'I like that, oopu undhiii ooopestaav 🔥';
+    } else if (resultScore == 20) {
+      resultText = 'Motham istam koncham kastam';
+    } else if (resultScore == 18) {
+      //6*3
+      resultText = 'Chill max 😎 tinnama padukunnama tellarindha';
+    } else if (resultScore == 16) {
+      resultText = 'Mast shades unnay ra neelo, eyyy kamal haasannn 👾';
+    } else if (resultScore >= 12 && resultScore <= 14) {
+      resultText = 'Koncham istam Koncham kastam';
     } else {
-      resultText = 'Hiya';
+      resultText = 'develop, develop? develop... develop... develop!';
     }
     return resultText;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(children: <Widget>[
-        Text(
-          resultPhrase,
-          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
-        TextButton(
-          child: Text('Restart Test!'),
-          style: ButtonStyle(
-              foregroundColor: MaterialStateProperty.all(Colors.blue)),
-          onPressed: resetHandler,
-        ),
-      ]),
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          OutlinedButton(
+            child: Text('Restart Test!'),
+            style: OutlinedButton.styleFrom(
+                primary: Colors.orange, side: BorderSide(color: Colors.orange)),
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
     );
   }
 }
